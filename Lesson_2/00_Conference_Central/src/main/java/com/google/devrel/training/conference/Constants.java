808768835004f36f996661fdd4babed4b2f9665a
unknown <irahavoi@wpg02333.obsglobal.com>
2015-06-19 12:06:58 -0500

    Finished lesson 2

--- Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/Constants.java
+++ Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/Constants.java
@@ -6,7 +6,7 @@ import com.google.api.server.spi.Constant;
  * Contains the client IDs and scopes for allowed clients consuming the conference API.
  */
 public class Constants {
-    public static final String WEB_CLIENT_ID = "replace this with your Web cliend ID";
+    public static final String WEB_CLIENT_ID = "53601643817-srinf241rqj3pqj0cii3mf5nkks4cod2.apps.googleusercontent.com";
     public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
     public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
     public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
--- Lesson_2/00_Conference_Central/src/main/webapp/js/app.js
+++ Lesson_2/00_Conference_Central/src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '53601643817-srinf241rqj3pqj0cii3mf5nkks4cod2.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
